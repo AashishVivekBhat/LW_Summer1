1aaf9c7e7c5a2b4861f54031d8ee7900989c21a1
AashishVivekBhat <72133255+AashishVivekBhat@users.noreply.github.com>
2021-05-30 13:31:28 +0530

    Update Model.py

--- Model.py
+++ Model.py
@@ -2,12 +2,15 @@ import pandas
 import numpy
 
 db = pandas.read_csv('SalaryData.csv')
-x = db['YearsExperience'].values.reshape(30,1)
-y = db['Salary'].values.reshape(30,1)
+x = db['YearsExperience']
+x = x.values.reshape(30,1)
+y = db['Salary']
+y = y.values.reshape(30,1)
 
 from sklearn.linear_model import LinearRegression
-Model = LinearRegression
+Model = LinearRegression()
 Model.fit(x,y)
+Model.coef_
 
 val = float(input("Enter the Experience : "))
 output = Model.predict( [[val]] )
